2b3191bda0e0796c40e98567c22d440b2d2656a1
Julio Cesar Fausto <jcfausto@gmail.com>
2015-11-26 14:18:57 -0200

    Fixed the output that now generates a json string

--- gpscrap.py
+++ gpscrap.py
@@ -2,10 +2,11 @@
 """Google Play Review Scrapper
 
 Usage:
-  gpscrap.py --appid=<google-play-application-id>
+  gpscrap.py --appid=<google-play-application-id> --pagenum=<page_num>
 
 Options:
   --appid=<app_id>	The ID of the app where the reviews will be scrapped.
+  --page=<page_num> The review page number.
 """
 from docopt import docopt
 import pycurl
@@ -18,6 +19,7 @@ if __name__ == '__main__':
 	arguments = docopt(__doc__, version='Google Play Review Scrapper V1.0')
 	
 	application_id = arguments["--appid"]
+	page_number = arguments["--pagenum"]
 
 	buffer = BytesIO()
 	c = pycurl.Curl()
@@ -25,7 +27,7 @@ if __name__ == '__main__':
 	c.setopt(c.URL, 'https://play.google.com/store/getreviews?authuser=0')
 	c.setopt(c.ENCODING, 'gzip,deflate')
 
-	post_data = {'reviewType': '0', 'pageNum': '2', 'id': application_id, 'reviewSortOrder': '0', 'xhr': '1', 'token': 'FxkOaC8-kGnB-duj_Zcc3H7BRow%3A1448462501595', 'hl': 'pt_BR'} 
+	post_data = {'reviewType': '0', 'pageNum': page_number, 'id': application_id, 'reviewSortOrder': '0', 'xhr': '1', 'token': 'FxkOaC8-kGnB-duj_Zcc3H7BRow%3A1448462501595', 'hl': 'pt_BR'} 
 	postfields = urlencode(post_data)
 
 	c.setopt(c.POSTFIELDS, postfields)
@@ -38,11 +40,13 @@ if __name__ == '__main__':
 
 	#you could try this expression here: http://pythex.org/
 	filtered = re.match(r'^\)]}\'\s*\[\["ecr",1,(?P<body>"[^"].*"),[0-9]]\s*]', body).group('body')
-	
-	soup = BeautifulSoup(filtered.decode('unicode-escape'))
-	filtered2 = ""
-	filtered2 = str(soup.findAll("div",id="review-body"))
-	filtered3 = filtered2.replace("<div id=\"review-body\"><spanclass>", "{\"review\": \"")
-	filtered4 = filtered3.replace("<div id=\"review-link\" style=\"display:none\"><aclass>Resenhacompleta</aclass></div></spanclass></div>", "\"}")
+	converted = filtered.decode('unicode-escape')
 
-	print(filtered4)
+	soup = BeautifulSoup(converted)
+	review_bodies = str(soup.findAll("div", {"class": "review-body"}))
+		
+	json_start = review_bodies.replace("<div class=\"review-body\"> <span class=\"review-title\">", "{\"review\": \"")
+	json_end = json_start.replace("<div class=\"review-link\" style=\"display:none\"> <a class=\"id-no-nav play-button tiny\" href=\"#\" target=\"_blank\">Resenha completa</a> </div> </div>", "\"}")
+	json_final = json_end.replace("</span>", "")
+
+	print(json_final)
